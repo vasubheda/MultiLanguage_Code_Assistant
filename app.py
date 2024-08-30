f894ecf036f0cd1e2e0429b795f63ba00c2a6660
vasubheda <vishwasvbheda@gmail.com>
2024-08-30 20:40:25 +0530

    Project completion

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import gradio as gr
+
+url="http://localhost:11434/api/generate"
+
+header={
+    'Content-Type':'application/json'
+}
+
+history=[]
+
+def generate_response(prompt):
+    history.append(prompt)
+    final_prompt="\n".join(history)
+    data={
+        "model":"codeguru",
+        "prompt":final_prompt,
+        "stream":False
+    }
+    response=requests.post(url,headers=header,data=json.dumps(data))
+    
+    if response.status_code==200:
+        response=response.text
+        data=json.loads(response)
+        actual_response=data['response']
+        return actual_response
+    else:
+        print("error:",response.text)
+        
+interface=gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Enter your prompt"),
+    outputs="text"
+)
+interface.launch()
